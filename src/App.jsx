66acfc15e6fdba03e60c2161d2f7c24b24063c3a
LuisaMSegur <165593500+LuisaMSegur@users.noreply.github.com>
2024-11-17 20:57:30 -0300

    primer marcado con React del proyecto Around usa

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import logo from "./images/logo.png";
+import avatar from "./images/LagoLouise.jpg";
+import buttonProfile from "./images/EditButton.png";
+import buttonEdit from "./images/AddButton.png";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="page">
+        <header className="header">
+          <img src={logo} alt="logo around USA" className="header__logo" />
+        </header>
+        <main className="content">
+          <section className="profile">
+            <div className="profile__container-photo">
+              <img
+                src={avatar}
+                alt="profile photo"
+                className="profile__photo"
+                id="avatar"
+              />
+            </div>
+            <h1 className="profile__title">Jacques Cousteau</h1>
+            <button className="profile__button">
+              <img
+                src={buttonProfile}
+                alt="edit button"
+                className="profile__button profile__button_edit"
+              />
+            </button>
+            <h2 className="profile__subtitle">Explorador</h2>
+            <button className="profile__button-add">
+              <img
+                src={buttonEdit}
+                alt="button add card"
+                className="profile__button-add profile__button-add_card"
+              />
+            </button>
+          </section>
+          <section className="cards"></section>
+        </main>
+        <footer className="footer">
+          <h3 className="footer__name">2024 Luisa M. Seguro</h3>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
